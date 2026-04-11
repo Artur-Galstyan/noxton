e871f88b3d224770fd3cb10b3d4b5f92b2578349
Artur Galstyan <galstyan.artu@gmail.com>
2026-04-11 14:37:36 +0200

    export summarise model fn

--- noxton/utils/__init__.py
+++ noxton/utils/__init__.py
@@ -1,3 +1,8 @@
-from .utils import default_floating_dtype, dtype_to_str, get_cache_path
+from .utils import default_floating_dtype, dtype_to_str, get_cache_path, summarize_model
 
-__all__ = ["default_floating_dtype", "dtype_to_str", "get_cache_path"]
+__all__ = [
+    "default_floating_dtype",
+    "dtype_to_str",
+    "get_cache_path",
+    "summarize_model",
+]
